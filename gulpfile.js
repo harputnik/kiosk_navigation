9e3085ae3707a300137d50348fda9ba351fc2f98
unknown <karolina.szuber@gmail.com>
2017-01-24 13:45:15 +0100

    dropdowns added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
+var minify = require('gulp-minify');
+
+gulp.task('compress', function() {
+  gulp.src('src/scripts/*.js')
+    .pipe(minify({
+        ext:{
+            src:'-debug.js',
+            min:'.min.js'
+        },
+    }))
+    .pipe(gulp.dest('dist/scripts'))
+});
 
 gulp.task('less', function () {
   return gulp.src('./src/style/main.less')
